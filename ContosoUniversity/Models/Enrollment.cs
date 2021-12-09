84f54aa2f8d10b367e280fe9bbe2ed76be1a0206
coderNet2021 <cnss.saida@gmail.com>
2021-12-10 00:01:25 +0200

    complex model done

--- ContosoUniversity/Models/Enrollment.cs
+++ ContosoUniversity/Models/Enrollment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,12 +12,12 @@ namespace ContosoUniversity.Models
         A, B, C, D, F
     }
 
-
     public class Enrollment
     {
         public int EnrollmentID { get; set; }
         public int CourseID { get; set; }
         public int StudentID { get; set; }
+        [DisplayFormat(NullDisplayText = "No grade")]
         public Grade? Grade { get; set; }
 
         public Course Course { get; set; }
